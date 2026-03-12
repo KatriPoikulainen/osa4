9cedd22bbe7b78eb398ac0800fa84df2c431172e
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-03-12 14:00:16 +0200

    blogilistan laajennus step6

--- blogilista/app.js
+++ blogilista/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -15,5 +16,6 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
